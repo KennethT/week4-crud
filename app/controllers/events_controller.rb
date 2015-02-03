3ec22c887bcf87e7d56e474d1bccea2c32e65c43
Kenneth To <kennethkto@gmail.com>
2015-02-02 17:01:13 -0700

    add table submission

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,30 @@
+class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def show
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def edit
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to events_path
+    else
+      render:new
+    end
+  end
+
+  def event_params
+    params.require(:event).permit(:date, :location, :description, :capacity, :requires_id)
+  end
+
+
+end
